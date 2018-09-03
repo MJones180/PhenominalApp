0e8d5637361f141511328947d0785e62ef080979
Michael Jones <jonesmichael3.1415@gmail.com>
2018-09-03 19:41:54 -0400

    2

--- src/utils/keys.js
+++ src/utils/keys.js
@@ -1,3 +1,3 @@
 export const facebookID = '702523006760063';
 export const googleID = '491743162746-k2760g0j38bp7gfjjorvsri518rg7684.apps.googleusercontent.com';
-export const stripeKey = __DEV__ ? 'pk_test_Ih0vsUKFYTokiNVLqDccfI5B' : 'pk_live_KDnJgend3cYOkqXRMU5FwhDW';
+export const stripeKey = __DEV__ ? 'pk_test_QnM3ibP2wCmCgTJEqb8ISw9e' : 'pk_live_KDnJgend3cYOkqXRMU5FwhDW';
