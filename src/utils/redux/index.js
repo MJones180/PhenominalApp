43e4c992c6cb1bcc960bbfeda5cd6790bc2f7f82
Michael Jones <jonesmichael3.1415@gmail.com>
2018-09-03 20:34:07 -0400

    6

--- src/utils/redux/index.js
+++ src/utils/redux/index.js
@@ -1,13 +1,7 @@
 import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import user from './user';
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-const store = createStore(
-  combineReducers({
-    user,
-  }),
-  devTools
-);
+const store = createStore(combineReducers({ user }), composeWithDevTools());
 
 export default store;
