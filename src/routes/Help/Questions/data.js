baece801b9c8058ce7e60397f2897168520db4b4
Michael Jones <jonesmichael3.1415@gmail.com>
2018-09-12 20:13:48 -0400

    22

--- src/routes/Help/Questions/data.js
+++ src/routes/Help/Questions/data.js
@@ -10,6 +10,31 @@ export default [{
     From there you can make donations based off of your balance.
   `,
 }, {
+  title: 'How do I add funds to my account?',
+  content: `
+    Money can be added to your account with the "Add Funds" buttons located throughout the site.
+    Please note, these buttons will not be visible if you are not signed in.
+    Currently, all payments will be processed through Stripe.
+  `,
+}, {
+  title: 'What is Stripe?',
+  content: `
+    Stripe is a highly secure payment processing platform that Phenominal uses to add funds to your account.
+    When adding funds to your account, Stripe will take 2.9% + 30¢ for processing fees.
+    The rest of the money will be added to your account's balance.
+    Example: if you add $10.00 to your account, Stripe will take $0.59 and $9.41 will be added to your account.
+  `,
+}, {
+  title: 'Does Phenominal keep any portion of my donation?',
+  content: `
+    No. Phenominal donates 100% of all donations received.
+  `,
+}, {
+  title: 'How will my donation be used?',
+  content: `
+    Donations will be appropriated by the charity at their discretion (please see pie chart on charity's page).
+  `,
+}, {
   title: 'How does the charity rotation work?',
   content: `
     Every Tuesday at 12:01 AM (UTC) new charities will be available.
@@ -30,20 +55,8 @@ export default [{
     Sorry for any inconveniences.
   `,
 }, {
-  title: 'How do I add funds to my account?',
-  content: `
-    Money can be added to your account with the "Add Funds" buttons located throughout the site.
-    Please note, these buttons will not be visible if you are not signed in.
-    Currently, all payments will be processed through Stripe.
-  `,
-}, {
   title: 'When are the proceeds donated to the charities?',
   content: `
     At the end of every month all of the money raised will be sent to the corresponding charities.
-  `,
-}, {
-  title: 'Does Phenominal keep any portion of the donation?',
-  content: `
-    No. Phenominal donates 100% of all donations received.
   `,
 }];
