1dad80d4ee7723bfb541f8c0d45899632e52c567
Michael Jones <jonesmichael3.1415@gmail.com>
2018-09-13 10:51:49 -0400

    26

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,2 +1,2 @@
-export const graphqlBase = __DEV__ ? 'http://localhost:4000' : 'https://server.phenominal.fund';
+export const graphqlBase = __DEV__ ? 'http://localhost:4000' : 'https://phenominal-production-server.herokuapp.com/';
 export const image = route => require(`images/${route}`);
