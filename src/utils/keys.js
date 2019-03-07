8bef1cf0398c3fa642751a8110ff3ff206019a92
Michael Jones <jonesmichael3.1415@gmail.com>
2019-03-06 20:24:40 -0500

    64

--- src/utils/keys.js
+++ src/utils/keys.js
@@ -1,4 +1,4 @@
 export const analyticsKey = 'UA-126263348-1';
 export const facebookID = '702523006760063';
 export const googleID = '456251720764-7lbpqg4btjq4s5r4d1uihjcetvldgakk.apps.googleusercontent.com';
-export const stripeKey = __DEV__ ? 'pk_test_QnM3ibP2wCmCgTJEqb8ISw9e' : 'pk_live_KDnJgend3cYOkqXRMU5FwhDW';
+export const stripeKey = __DEV__ ? 'pk_test_Ih0vsUKFYTokiNVLqDccfI5B' : 'pk_live_5h4TGpCkiPQK6bWIFFgDdEEs';
