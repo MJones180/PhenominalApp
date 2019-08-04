32951fdb79118987b418bf45d9b43759f52ee34d
Michael Jones <jonesmichael3.1415@gmail.com>
2019-08-04 16:30:09 -0400

    101

--- src/routes/Balance/index.js
+++ src/routes/Balance/index.js
@@ -9,28 +9,20 @@ export default Container(({ balance, expirations }) => (
   <main className={styles.container}>
     <HeadTags />
     <div className={styles.buttons}>
-      <div className={styles.addFunds}>
-        <h4>Low Balance?</h4>
-        <Button to="/add-funds" special className={styles.button}>
-          Add Funds
-        </Button>
-      </div>
-      <div className={styles.transactions}>
-        <h4>View history</h4>
-        <Button to="/transactions" className={styles.button}>
-          Transactions
-        </Button>
-      </div>
+      <Button to="/add-funds" special>
+        Add Funds
+      </Button>
+      <Button to="/transactions" className={styles.buttonMargin}>
+        Transactions
+      </Button>
     </div>
     <div className={styles.balance}>
-      <div>
-        <h3>
-          Balance
-        </h3>
-        <h1>
-          {balance}
-        </h1>
-      </div>
+      <h2>
+        Account Balance
+      </h2>
+      <h1>
+        {balance}
+      </h1>
     </div>
     <div className={styles.expiration}>
       {_.map(expirations, ({ amount, days, expiration }, key) => (
