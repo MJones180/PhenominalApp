afe26c036533d6cf44d8a449c89df5316784fe18
Michael Jones <jonesmichael3.1415@gmail.com>
2019-08-08 16:34:16 -0400

    102

--- src/routes/AddFunds/Form/Result/container.js
+++ src/routes/AddFunds/Form/Result/container.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import _ from 'lodash';
 import { currency } from 'utils/number';
 
 export default Component => (
   ({ amount }) => {
     // The amount of money the user will actually receieve
-    // Stripe takes 2.9% + 30 cents
-    let processed = amount - (amount * 0.029) - 30;
+    // Stripe takes 2.9% + 30 cents and rounds the fee
+    let processed = amount - _.round((amount * 0.029) + 30);
     // Do not show negative numbers
     if (processed < 0) processed = 0;
     return (
