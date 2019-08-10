fa34e8b4e10c838c0dd48e9ef324b93ba38bc4ff
Michael Jones <jonesmichael3.1415@gmail.com>
2019-08-10 16:38:26 -0400

    105

--- src/app/Analytics.js
+++ src/app/Analytics.js
@@ -1,15 +1,17 @@
-import { initialize, pageview } from 'react-ga';
+import GoogleAnalytics from 'react-ga';
 import { withRouter } from 'react-router-dom';
 import { analyticsKey } from 'utils/keys';
+
+// Startup Google Analytics
+GoogleAnalytics.initialize(analyticsKey);
 
 // Listen for route changes
 export default withRouter(({ location: { pathname } }) => {
   // Only use analytics in production
   if (__PROD__) {
-    // Startup Google Analytics
-    initialize(analyticsKey);
     // Update the current page
-    pageview(pathname);
+    GoogleAnalytics.set({ page: pathname });
+    GoogleAnalytics.pageview(pathname);
   }
   // Necessary for a valid React component
   return false;
